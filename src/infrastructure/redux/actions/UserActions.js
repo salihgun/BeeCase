90f3e386dbc845e7ec65b105409716c134937227
salihgun <salihgun97@outlook.com>
2022-07-17 02:10:25 +0300

    Fix: performance improvements

--- src/infrastructure/redux/actions/UserActions.js
+++ src/infrastructure/redux/actions/UserActions.js
@@ -1,18 +1,21 @@
-import axios from 'axios';
-import {setIsRefreshing, setUsers, addUsers} from '../reducers/UserReducer';
+import axios from 'axios'
+import {setIsRefreshing, setUsers, addUsers} from '../reducers/UserReducer'
+import {batch} from 'react-redux'
 
 export const getUsers = async dispatch => {
-  dispatch(setIsRefreshing(true));
-  const response = await axios.get('https://randomuser.me/api/?results=20');
+  dispatch(setIsRefreshing(true))
+  const response = await axios.get('https://randomuser.me/api/?results=20')
   if (response.data) {
-    dispatch(setUsers(response.data.results));
-    dispatch(setIsRefreshing(false));
+    batch(() => {
+      dispatch(setUsers(response.data.results))
+      dispatch(setIsRefreshing(false))
+    })
   }
-};
+}
 
 export const loadMoreUser = async dispatch => {
-  const response = await axios.get('https://randomuser.me/api/?results=20');
+  const response = await axios.get('https://randomuser.me/api/?results=20')
   if (response.data) {
-    dispatch(addUsers(response.data.results));
+    dispatch(addUsers(response.data.results))
   }
-};
+}
